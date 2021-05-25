11f7b09c022a1a15b53b56bc2bd3929931d5f3e8
bishal <bishal@cramstack.com>
2021-05-25 16:35:29 +0600

    event launch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,75 +1,24 @@
-$(document).ready(function () {
-  $('.client-wrapper').owlCarousel({
-    loop: true,
-    autoplay: true,
-    lazyLoad: true,
-    autoplayTimeout: 1520,
-    smartSpeed: 1500,
-    animateIn: 'linear',
-    animateOut: 'linear',
-    responsiveClass: true,
-    autoplayHoverPause: true,
-    center: true,
-    stagePadding: 50,
-    navText: [
-      '<i class="bi bi-chevron-left"></i>',
-      '<i class="bi bi-chevron-right"></i>',
-    ],
-    nav: true,
-    responsive: {
-      0: {
-        items: 1,
-        autoWidth: true,
-      },
-      600: {
-        items: 4,
-        autoWidth: true,
-      },
-      1000: {
-        items: 8,
-      },
-    },
-  });
-});
+let countDownDate = new Date('December 10, 2022 15:37:25').getTime();
 
-$(document).ready(function () {
-  $('.testimonial-wrapper').owlCarousel({
-    loop: true,
-    autoplay: true,
-    items: 1,
-    lazyLoad: true,
-    autoplayTimeout: 3000,
-    smartSpeed: 1500,
-    center: true,
-    navText: [
-      '<i class="bi bi-arrow-left"></i>',
-      '<i class="bi bi-arrow-right"></i>',
-    ],
-    nav: true,
-    autoplayHoverPause: true,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false,
-      },
-      600: {
-        items: 1,
-      },
-      1000: {
-        items: 1,
-      },
-    },
-  });
-});
+const x = setInterval(function () {
+  let now = new Date().getTime();
+  let diff = countDownDate - now;
 
-$(function () {
-  $('.popup-video-one').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-    fixedContentPos: false,
-  });
-});
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  document.getElementById('days').innerHTML = days;
+  document.getElementById('hours').innerHTML = hours;
+  document.getElementById('mins').innerHTML = minutes;
+  document.getElementById('sec').innerHTML = seconds;
+
+  if (diff < 0) {
+    clearInterval(x);
+    document.getElementById('days').innerHTML = 0;
+    document.getElementById('hours').innerHTML = 0;
+    document.getElementById('mins').innerHTML = 0;
+    document.getElementById('sec').innerHTML = 0;
+  }
+}, 1000);
---
+++ assets/js/plugins.js
@@ -0,0 +1,75 @@
+$(document).ready(function () {
+  $('.client-wrapper').owlCarousel({
+    loop: true,
+    autoplay: true,
+    lazyLoad: true,
+    autoplayTimeout: 1520,
+    smartSpeed: 1500,
+    animateIn: 'linear',
+    animateOut: 'linear',
+    responsiveClass: true,
+    autoplayHoverPause: true,
+    center: true,
+    stagePadding: 50,
+    navText: [
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>',
+    ],
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+        autoWidth: true,
+      },
+      600: {
+        items: 4,
+        autoWidth: true,
+      },
+      1000: {
+        items: 8,
+      },
+    },
+  });
+});
+
+$(document).ready(function () {
+  $('.testimonial-wrapper').owlCarousel({
+    loop: true,
+    autoplay: true,
+    items: 1,
+    lazyLoad: true,
+    autoplayTimeout: 3000,
+    smartSpeed: 1500,
+    center: true,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>',
+    ],
+    nav: true,
+    autoplayHoverPause: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
+});
+
+$(function () {
+  $('.popup-video-one').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false,
+  });
+});
